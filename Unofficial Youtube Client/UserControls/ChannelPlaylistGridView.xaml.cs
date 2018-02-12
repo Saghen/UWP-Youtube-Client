9a6d6d8c280a5fd33b9589aae9bb2cc9f6ae99ef
Saghen <liamcdyer@gmail.com>
2018-02-12 01:36:46 -0500

    Working channel playlist scrolling

--- Unofficial Youtube Client/UserControls/ChannelPlaylistGridView.xaml.cs
+++ Unofficial Youtube Client/UserControls/ChannelPlaylistGridView.xaml.cs
@@ -12,6 +12,7 @@ using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
 using Windows.UI.Xaml.Navigation;
 using YTApp.Classes;
 using YTApp.Classes.EventsArgs;
@@ -23,14 +24,65 @@ namespace YTApp
     public sealed partial class ChannelPlaylistGridView : UserControl
     {
         public event EventHandler<RoutedEventArgsWithID> ItemClicked;
-        public int index = 0;
+        private static int xTransform = 0;
 
         public ChannelPlaylistGridView(List<YoutubeItemDataType> list, string header)
         {
             this.InitializeComponent();
-            VideoItemGridView.Header = header;
-            this.Height = Double.NaN;
-            VideoItemGridView.ItemsSource = new ObservableCollection<YoutubeItemDataType>(list);
+            VideoItemHeader.Header = header;
+
+            foreach (var item in list)
+            {
+                var parent = new GridViewItem()
+                {
+                    Height = 250,
+                    Width = 250
+                };
+
+                var stkPanel = new StackPanel()
+                {
+                    Margin = new Thickness(10)
+                };
+
+                var img = new Image()
+                {
+                    Margin = new Thickness(10),
+                    HorizontalAlignment = HorizontalAlignment.Center,
+                    Source = new BitmapImage(new Uri(item.Thumbnail))
+                };
+
+                var title = new TextBlock()
+                {
+                    Text = item.Title,
+                    FontSize = 15,
+                    TextWrapping = TextWrapping.WrapWholeWords,
+                    TextTrimming = TextTrimming.CharacterEllipsis,
+                    HorizontalAlignment = HorizontalAlignment.Center
+                };
+
+                var author = new TextBlock()
+                {
+                    Text = item.Author,
+                    FontSize = 12,
+                    HorizontalAlignment = HorizontalAlignment.Center
+                };
+
+                var viewsAndDate = new TextBlock()
+                {
+                    Text = item.ViewsAndDate,
+                    FontSize = 12,
+                    HorizontalAlignment = HorizontalAlignment.Center
+                };
+
+                stkPanel.Children.Add(img);
+                stkPanel.Children.Add(title);
+                stkPanel.Children.Add(author);
+                stkPanel.Children.Add(viewsAndDate);
+
+                parent.Content = stkPanel;
+
+                VideoItems.Children.Add(parent);
+            }
         }
 
         private void VideoItemGridView_ItemClick(object sender, ItemClickEventArgs e)
@@ -38,6 +90,19 @@ namespace YTApp
             var item = (YoutubeItemDataType)e.ClickedItem;
             if (ItemClicked != null)
                 ItemClicked(this, new RoutedEventArgsWithID(item.Id));
+        }
+
+        private void MoveRight_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            xTransform += -500;
+            VideoItems.RenderTransform = new TranslateTransform() { X = xTransform };
+        }
+
+        private void MoveLeft_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            if (xTransform <= -500)
+                xTransform += 500;
+            VideoItems.RenderTransform = new TranslateTransform() { X = xTransform };
         }
     }
 }
